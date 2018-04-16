91e31a70f5fcbc026b8bdbc947d5d82bd3515112
Gandhi D <gandhi.d@prodapt.com>
2018-04-16 16:54:54 +0530

    port issues fixed

--- src/main/java/com/prodapt/quiz/Application.java
+++ src/main/java/com/prodapt/quiz/Application.java
@@ -1,8 +1,6 @@
 package main.java.com.prodapt.quiz;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.LogManager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,9 +10,6 @@ public class Application {
 
     public static void main(String[] args) throws SecurityException, IOException {
     	
-    	InputStream inputStream=Application.class.getResourceAsStream("log4j.properties");
-    	LogManager.getLogManager().readConfiguration(inputStream);
-    	System.getProperties().put("server.port", 88);
         SpringApplication.run(Application.class, args);
     }
 }
